fde900ee5167cbd56ef9e57c2c9aebf2f23abfb4
Jinto Jose <jintoppy@gmail.com>
2022-07-02 15:37:09 +0200

    adding dropdown component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { LayoutComponent } from './components/layout/layout.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { DropdownComponent } from './components/dropdown/dropdown.component';
 
 @NgModule({
-  declarations: [LayoutComponent, NavbarComponent, HeaderComponent],
+  declarations: [LayoutComponent, NavbarComponent, HeaderComponent, DropdownComponent],
   imports: [CommonModule, MatToolbarModule],
   exports: [LayoutComponent, HeaderComponent]
 })
