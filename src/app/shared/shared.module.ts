2c93d77cbfdb39b71b2dddabc7049d72777b0787
Jinto Jose <jintoppy@gmail.com>
2022-07-02 14:49:30 +0200

    initial

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './components/layout/layout.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+
+@NgModule({
+  declarations: [
+    LayoutComponent,
+    NavbarComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    LayoutComponent,
+    HeaderComponent
+  ]
+})
+export class SharedModule { }
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    UserRoutingModule
+  ]
+})
+export class UserModule { }
