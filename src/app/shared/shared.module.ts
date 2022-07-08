ce87a45e862aadc7a1170ce147ad9af408842f0c
Jinto Jose <jintoppy@gmail.com>
2022-07-08 07:35:13 +0200

    [GTP-40] Box Container component (#8)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,27 +2,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { LayoutComponent } from './components/layout/layout.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DropdownComponent } from './components/dropdown/dropdown.component';
+import { BoxContainerComponent } from './components/box-container/box-container.component';
 
 @NgModule({
   declarations: [
     LayoutComponent,
     NavbarComponent,
     HeaderComponent,
-    DropdownComponent
+    DropdownComponent,
+    BoxContainerComponent
   ],
   imports: [
     CommonModule,
     MatToolbarModule,
     MatIconModule,
+    MatCardModule,
     MatButtonModule,
     RouterModule.forChild([])
   ],
-  exports: [LayoutComponent, HeaderComponent]
+  exports: [LayoutComponent, HeaderComponent, BoxContainerComponent]
 })
 export class SharedModule {}
