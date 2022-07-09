171203b7fe39a385923e68ff82f1ccd1ffecc579
sandeep-pinasimham <108320324+sandeep-pinasimham@users.noreply.github.com>
2022-07-09 16:00:35 +0530

    [GTP-40] Added Box-container (#12)

--- src/app/shared/components/box-container/box-container.component.ts
+++ src/app/shared/components/box-container/box-container.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+interface DropDownData {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-box-container',
   templateUrl: './box-container.component.html',
   styleUrls: ['./box-container.component.scss']
 })
 export class BoxContainerComponent implements OnInit {
+  foods: DropDownData[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' }
+  ];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    console.log('inside BoxContainerComponent ngOnInit');
+    console.log('inside box-container');
   }
-
 }
